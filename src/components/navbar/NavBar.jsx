c5fa6162f56786d54554aa4a1c122e6d0e0c8e7a
Chebel405 <anne.chebel@gmail.com>
2023-10-11 16:57:36 +0200

    NavBar en cours

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark m-0 p-0">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Auchan</a>
+            </div>
+        </nav>
+    )
+}
