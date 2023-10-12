4d3cdc927ddd705412ee1a7cda375ad76d4890b9
Chebel405 <anne.chebel@gmail.com>
2023-10-12 14:09:20 +0200

    Création du _listProduit.scss

--- src/components/ListProduit/listProduit.jsx
+++ src/components/ListProduit/listProduit.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Produits } from "../../Data/Produits"; // Utilisation de la première lettre en majuscule
+import styles from './../../assets/styles/_listProduit.scss';
 
 const ListProduit = () => {
+    const produitsParLigne = 3; // Nombre de produits par ligne
+
     return (
         <div>
             <h1>Liste des Produits</h1>
-            {Produits.map((produit) => ( // Utilisation de la première lettre en majuscule
-                <div key={produit.id}>
-                    <h2>{produit.nom}</h2>
-                    <p>Prix : {produit.prix} Euros</p>
-                    <p>Type : {produit.type}</p>
-                </div>
-            ))}
+            <div className="ligne-produits">
+                {Produits.map((produit) => (
+                    <div key={produit.id} className="produit">
+                        <h2>{produit.nom}</h2>
+                        <p>Prix : {produit.prix} Euros</p>
+                        <p>Type : {produit.type}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
