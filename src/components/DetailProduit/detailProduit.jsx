12ec717c8e2f25123a5962c41cf84d9bc03c0e74
Chebel405 <anne.chebel@gmail.com>
2023-10-13 14:00:12 +0200

    Ajout bouton retour dans detailProduit

--- src/components/DetailProduit/detailProduit.jsx
+++ src/components/DetailProduit/detailProduit.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { produits } from "../../Data/Produits";
 import styles from './../../assets/styles/detailProduit.css';
+import { Link } from 'react-router-dom';
+
 
 const DetailProduit = () => {
     const { id } = useParams(); // Récuperer l'Id avec l'URL
@@ -17,6 +19,7 @@ const DetailProduit = () => {
             <h2>{produit.nom}</h2>
             <p>Prix : {produit.prix} Euros</p>
             <p>Type : {produit.type}</p>
+            <Link to="/" className="btn-retour">Retour</Link>
         </div>
     );
 };
